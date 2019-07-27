0d6fd631d40a1b310c26c127001b422b38973066
Jorge Martín <tcbpepo@gmail.com>
2019-07-27 12:44:58 +0200

    chore: hide x-powered-by header

--- connection.js
+++ connection.js
@@ -40,6 +40,7 @@ function configureExpress(db) {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(morgan('dev'));
+  app.disable('x-powered-by');  
   i18n.configure({
     defaultLocale: 'en_US',
     fallbacks:{'es': 'es_ES', 'en': 'en_US'},
